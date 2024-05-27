aef86838168411ba62c95fdc30dad1db5f0183e8
berryboostbb <berryboostbb@gmail.com>
2024-05-27 23:45:50 +0500

    route changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import logo from "@/assets/Grazle Logo.png";
@@ -6,8 +7,15 @@ import Cart from "@/assets/Cart.png";
 import Seller from "@/assets/Vector.png";
 import MenuIcon from "@/assets/VectorMenu.png";
 import { Avatar } from "@mui/material";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Navbar() {
+  const router = useRouter();
+
+  const hanldeGoToLogin = () => {
+    router.push("/Login");
+  };
   return (
     <div className="mx-[16px] lg:mx-[150px] md:mx-[60px] my-[16px] flex flex-col md:flex-wrap sm:flex-row sm:items-center">
       <div className="flex items-center justify-between w-full md:w-auto sm:w-auto">
@@ -19,7 +27,11 @@ export default function Navbar() {
           />
           <Image src={logo} alt="Logo" className="w-[80px] h-[47px]" />
         </div>
-        <div className="hidden lg:flex items-center color-[#393A44] text-[14px] font-normal mx-[64px]">
+
+        <Link
+          href="/"
+          className="hidden lg:flex items-center color-[#393A44] text-[14px] font-normal mx-[64px]"
+        >
           <p className="mr-[24px]">Home</p>
           <div className="border-l-[1px] border-r-[1px] border-[#D2D4DA]">
             <p className="mx-[24px]">Offers</p>
@@ -27,7 +39,7 @@ export default function Navbar() {
           <div>
             <p className="ml-[24px]">Categories</p>
           </div>
-        </div>
+        </Link>
         <div className="hidden lg:flex mr-[64px] md:ml-auto">
           <div className="relative w-[430px]">
             {/* Change the width of the input for small screens */}
@@ -57,7 +69,10 @@ export default function Navbar() {
             <p className="hidden sm:block lg:block">Become Seller</p>
           </div>
           {/* <Avatar className="w-[44px] h-[44px] ml-[16px]" /> */}
-          <button className=" bg-[#F70000] rounded-2xl h-[40px]  w-[151px] text-[14px] font-medium text-white">
+          <button
+            className=" bg-[#F70000] rounded-2xl h-[40px]  w-[151px] text-[14px] font-medium text-white"
+            onClick={() => hanldeGoToLogin()}
+          >
             Before Login
           </button>
         </div>
