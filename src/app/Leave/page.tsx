c102e3e9ad6cf1077cb82a1d665adefd92917caf
berryboostbb <berryboostbb@gmail.com>
2024-05-28 15:57:02 +0500

    fixes

--- src/app/Leave/page.tsx
+++ src/app/Leave/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import { IoLockClosed } from "react-icons/io5";
--- src/app/Payment/page.tsx
+++ src/app/Payment/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Radio } from "@mui/material";
 import React, { useState } from "react";
 import Image from "next/image";
